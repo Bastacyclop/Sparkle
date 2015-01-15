6f5589180e2f2d245b6a1918c35d5b20f248426e
Thomas Koehler <basta.t.k+git@gmail.com>
2015-01-15 22:44:48 +0100

    Draft entity module documentation

--- sparkle/src/entity/mod.rs
+++ sparkle/src/entity/mod.rs
@@ -9,8 +9,10 @@ pub use self::tag::TagMap;
 pub mod group;
 pub mod tag;
 
+/// A plain entity identifier.
 pub type Entity = usize;
 
+/// Represents an entity and its features.
 #[derive(PartialEq, Eq, Clone)]
 pub struct MetaEntity {
     pub entity: Entity,
@@ -20,7 +22,8 @@ pub struct MetaEntity {
 }
 
 impl MetaEntity {
-    pub fn new(entity: Entity) -> MetaEntity {
+    /// Creates a bare `MetaEntity`.
+    fn new(entity: Entity) -> MetaEntity {
         MetaEntity {
             entity: entity,
             tag: None,
@@ -29,7 +32,8 @@ impl MetaEntity {
         }
     }
 
-    pub fn reset(mut self) -> MetaEntity {
+    /// Resets the `MetaEntity`.
+    fn reset(mut self) -> MetaEntity {
         self.components.clear();
         self.tag = None;
         self.groups.clear();
@@ -38,6 +42,7 @@ impl MetaEntity {
     }
 }
 
+/// Maps entities using plain `Entity` identifiers, tags and groups.
 pub struct EntityMapper {
     mentities: MetaEntityMap,
     groups: GroupMap,
@@ -45,6 +50,7 @@ pub struct EntityMapper {
 }
 
 impl EntityMapper {
+    /// Creates a new `EntityMapper`.
     pub fn new() -> EntityMapper {
         EntityMapper {
             mentities: MetaEntityMap::new(),
@@ -53,10 +59,14 @@ impl EntityMapper {
         }
     }
 
+    /// Creates a new entity.
     pub fn create_entity(&mut self) -> Entity {
         self.mentities.create()
     }
 
+    /// Removes an entity.
+    ///
+    /// The removal event is recorded and will be notified before any system update.
     pub fn remove_entity(&mut self, entity: Entity) {
         {
             let mentity = self.mentities.get(entity);
@@ -66,38 +76,47 @@ impl EntityMapper {
         self.mentities.remove(entity);
     }
 
+    /// Returns a reference to the meta entity.
     pub fn get_mentity(&self, entity: Entity) -> &MetaEntity {
         self.mentities.get(entity)
     }
 
+    /// Returns a mutable reference to the meta entity.
     pub fn get_mentity_mut(&mut self, entity: Entity) -> &mut MetaEntity {
         self.mentities.get_mut(entity)
     }
 
+    /// Inserts an entity into a group.
     pub fn set_group(&mut self, entity: Entity, group: &str) {
         self.groups.insert_in(self.mentities.get_mut(entity), group);
     }
 
+    /// Removes an entity from a group.
     pub fn unset_group(&mut self,  entity: Entity, group: &str) {
         self.groups.remove_from(self.mentities.get_mut(entity), group);
     }
 
+    /// Returns an entity group as a vector.
     pub fn get_group(&mut self, group: &str) -> Vec<Entity> {
         self.groups.get(group)
     }
 
+    /// Sets an entity tag.
     pub fn set_tag(&mut self, tag: &str, entity: Entity) {
         self.tags.insert(self.mentities.get_mut(entity), tag);
     }
 
+    /// Unsets an entity tag.
     pub fn unset_tag(&mut self, entity: Entity) {
         self.tags.remove(self.mentities.get_mut(entity))
     }
 
+    /// Returns the entity tagged by `tag` if it exists.
     pub fn get_tag(&self, tag: &str) -> Option<Entity> {
         self.tags.get(tag)
     }
 
+    /// Notify all entity events that occurred to an observer.
     pub fn notify_events<O>(&mut self, cm: &mut ComponentMapper, obs: &mut O) where O: EntityObserver {
         let EntityMapper { ref mut mentities, .. } = *self;
 
@@ -113,20 +132,26 @@ impl EntityMapper {
     }
 }
 
+/// An event that occurred to a certain entity.
 type Event = (EventKind, Entity);
 
+/// The different kinds of entity-related events that can occur.
 #[derive(Copy, Show)]
 enum EventKind {
     Changed,
     Removed
 }
 
+/// A queue recording entity-related events.
+///
+/// Change events are protected against duplicates.
 struct EventQueue {
     changed_set: HashSet<Entity>,
     events: RingBuf<Event>
 }
 
 impl EventQueue {
+    /// Creates an empty `EventQueue`.
     fn new() -> EventQueue {
         EventQueue {
             changed_set: HashSet::new(),
@@ -134,16 +159,19 @@ impl EventQueue {
         }
     }
 
+    /// Records the change of an entity, ignoring duplicates.
     fn changed(&mut self, entity: Entity) {
         if self.changed_set.insert(entity) {
             self.events.push_back((EventKind::Changed, entity))
         }
     }
 
+    /// Records the removal of an entity. 
     fn removed(&mut self, entity: Entity) {
         self.events.push_back((EventKind::Removed, entity))
     }
 
+    /// Drains all recorded events.
     fn drain(&mut self) -> EventDrain {
         self.changed_set.clear();
         self.events.drain()
@@ -152,8 +180,11 @@ impl EventQueue {
 
 type EventDrain<'a> = ring_buf::Drain<'a, Event>;
 
+/// Observes entity-related events.
 pub trait EntityObserver {
+    /// Notifies the observer that an entity was changed.
     fn notify_changed(&mut self, mentity: &MetaEntity);
+    /// Notifies the observer that an entity was removed.
     fn notify_removed(&mut self, mentity: &MetaEntity);
 }
 
@@ -171,6 +202,7 @@ macro_rules! get_mentity_mut {
     )
 }
 
+/// A map of meta entities.
 struct MetaEntityMap {
     pool: Pool,
     mentities: VecMap<MetaEntity>,
@@ -178,6 +210,7 @@ struct MetaEntityMap {
 }
 
 impl MetaEntityMap {
+    /// Creates a new map of meta entities.
     fn new() -> MetaEntityMap {
         MetaEntityMap {
             pool: Pool::new(),
@@ -186,6 +219,7 @@ impl MetaEntityMap {
         }
     }
 
+    /// Creates a new entity.
     fn create(&mut self) -> Entity {
         let meta_entity = self.pool.get();
         let entity = meta_entity.entity;
@@ -195,19 +229,28 @@ impl MetaEntityMap {
         entity
     }
 
+    /// Removes an entity.
+    ///
+    /// The removal event is recorded and will be treated later.  
+    /// The entity effective removal is delayed until then.
     fn remove(&mut self, entity: Entity) {
         self.events.removed(entity);
     }
 
+    /// Returns a reference to a meta entity.
     fn get(&self, entity: Entity) -> &MetaEntity {
         get_mentity!(self.mentities, entity)
     }
 
+    /// Returns a mutable reference to a meta entity.
     fn get_mut(&mut self, entity: Entity) -> &mut MetaEntity {
         self.events.changed(entity);
         get_mentity_mut!(self.mentities, entity)
     }
 
+    /// Drains the entity-related events, applying `func` for each event.
+    ///
+    /// In case of a removal event, this is where the effective removal occurs.
     fn drain_events_with<'a, F>(&'a mut self, mut func: F)
         where F: for<'b> FnMut((EventKind, &'b MetaEntity))
     {
@@ -221,12 +264,16 @@ impl MetaEntityMap {
     }
 }
 
+/// A pool from where we can draw bare entities.
+///
+/// A drawn entity can be put back in to be recycled.
 struct Pool {
     available: Vec<MetaEntity>,
     next_id: usize
 }
 
 impl Pool {
+    /// Creates an empty `Pool`.
     fn new() -> Pool {
         Pool {
             available: Vec::new(),
@@ -234,6 +281,7 @@ impl Pool {
         }
     }
 
+    /// Retrieves a bare entity from the pool.
     fn get(&mut self) -> MetaEntity {
         self.available.pop().unwrap_or_else(|| {
             let id = self.next_id;
@@ -243,6 +291,7 @@ impl Pool {
         }).reset()
     }
 
+    /// Puts an entity back in the pool.
     fn put(&mut self, entity: MetaEntity) {
         self.available.push(entity);
     }
