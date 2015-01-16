00634d0cf1f45d5d1f4320b5bbc2da5ac4980d95
Thomas Koehler <basta.t.k+git@gmail.com>
2015-01-16 21:35:22 +0100

    Review blackboard documentation

--- sparkle/src/blackboard.rs
+++ sparkle/src/blackboard.rs
@@ -1,4 +1,4 @@
-//! The Blackboard related structures
+//! The blackboard related structures.
 
 use std::collections::HashMap;
 use std::collections::hash_map::Entry as HashMapEntry;
@@ -6,11 +6,10 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::any::Any;
 
-/// An `BlackboardEntry` is any value contained by the `Blackboard`
+/// Contains a value exposed by the `Blackboard`.
 pub type BlackboardEntry<T: 'static> = Rc<RefCell<T>>;
 
-/// A Blackboard is a container that can expose different values
-/// to multiples readers. 
+/// A container exposing different values to multiples readers. 
 ///
 /// This is generally used when multiple systems need to work together.
 pub struct Blackboard {
@@ -18,36 +17,36 @@ pub struct Blackboard {
 }
 
 impl Blackboard {
-    /// Create a new empty `Blackboard`
+    /// Creates an empty `Blackboard`.
     pub fn new() -> Blackboard {
         Blackboard {
             entries: HashMap::new()
         }
     }
 
-    /// Insert a value in the blackboard with the given name.
+    /// Inserts an entry in the blackboard with the given name and value.
     /// 
-    /// This method panic if the name is already used.
-    pub fn insert<T: 'static>(&mut self, name: &str, entry: T) {
+    /// This method panics if the name is already used.
+    pub fn insert<T: 'static>(&mut self, name: &str, value: T) {
         match self.entries.entry(name.to_string()) {
             HashMapEntry::Vacant(vacant) => {
-                vacant.insert(Box::new(Rc::new(RefCell::new(entry))));
+                vacant.insert(Box::new(Rc::new(RefCell::new(value))));
             },
             HashMapEntry::Occupied(_) => panic!("The name {} is already used", name)
         }
     }
 
-    /// Try to retrieve a value with the given name from the blackboard.
+    /// Tries to retrieve an entry from the blackboard with the given name.
     ///
-    /// Returns None if the value doesn't exist.
+    /// Returns `None` if the entry doesn't exist.
     pub fn try_get<T: 'static>(&self, name: &str) -> Option<BlackboardEntry<T>> {
         self.entries.get(name).and_then(|any_entry| any_entry.downcast_ref::<BlackboardEntry<T>>())
                               .map(|entry| entry.clone())
     }
 
-    /// Retrieve a value with the given name from the blackboard.
+    /// Retrieves an entry from the blackboard with the given name.
     ///
-    /// This method panic if the value doesn't exist.
+    /// This method panics if the entry doesn't exist.
     pub fn get<T: 'static>(&self, name: &str) -> BlackboardEntry<T> {
         let any_entry = self.entries.get(name).expect(format!("missing entry {}.", name).as_slice());
         let entry = any_entry.downcast_ref::<BlackboardEntry<T>>().expect("invalid entry type.");
@@ -56,38 +55,31 @@ impl Blackboard {
     }
 }
 
-/// A `SharedBlackboard` is a simple wrapper around
-/// a `Blackboard` allocated with Rc. 
+/// A simple wrapper around a `Blackboard`, allocated with Rc. 
 ///
-/// This permit to reduce the verbosity created by Rc and RefCell.
+/// This permits to reduce the verbosity created by Rc and RefCell.
 #[derive(Clone)]
 pub struct SharedBlackboard(Rc<RefCell<Blackboard>>);
 
 impl SharedBlackboard {
-    /// Create a new empty `SharedBlackboard`.
+    /// Creates an empty `SharedBlackboard`.
     pub fn new() -> SharedBlackboard {
         SharedBlackboard(Rc::new(RefCell::new(Blackboard::new())))
     }
 
-    /// Insert a value in the blackboard with the given name.
-    /// 
-    /// This method panic if the name is already used.
+    /// Behaves like [the original](struct.Blackboard.html#method.insert).
     #[inline]
     pub fn insert<T: 'static>(&mut self, name: &str, entry: T) {
         self.0.borrow_mut().insert(name, entry);
     }
 
-    /// Try to retrieve a value with the given name from the blackboard.
-    ///
-    /// Returns None if the value doesn't exist.
+    /// Behaves like [the original](struct.Blackboard.html#method.try_get).
     #[inline]
     pub fn try_get<T: 'static>(&self, name: &str) -> Option<BlackboardEntry<T>> {
         self.0.borrow().try_get(name)
     }
 
-    /// Retrieve a value with the given name from the blackboard.
-    ///
-    /// This method panic if the value doesn't exist.
+    /// Behaves like [the original](struct.Blackboard.html#method.get).
     #[inline]
     pub fn get<T: 'static>(&self, name: &str) -> BlackboardEntry<T> {
         self.0.borrow().get(name)
