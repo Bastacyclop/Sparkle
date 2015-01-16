be3b6f9c45e544b8ee111904d8d7d45f3d058d4e
Liotitch <rph.vaillant@gmail.com>
2015-01-16 16:41:07 +0100

    Add command doc

--- sparkle/src/command.rs
+++ sparkle/src/command.rs
@@ -1,17 +1,26 @@
+//! The Command related types
+
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::collections::RingBuf;
 
+/// The Command trait represent any command
+/// that can be runned.
 pub trait Command<Args>: 'static {
     fn run(&mut self, args: &mut Args);
 }
 
+/// Create a new stream of commands.
+/// Note that this is not thread safe.
 pub fn stream<Args>() -> (CommandSender<Args>, CommandReceiver<Args>) {
     let buffer = Rc::new(RefCell::new(CommandBuffer::new()));
 
     (CommandSender(buffer.clone()), CommandReceiver(buffer.clone()))
 }
 
+/// A `CommandSender` is a type that send commands
+/// to the `CommandReceiver`.
+/// You can have multiple sender using the clone method.
 pub struct CommandSender<Args>(SharedBuffer<Args>);
 
 impl<Args> Clone for CommandSender<Args> {
@@ -21,6 +30,7 @@ impl<Args> Clone for CommandSender<Args> {
 }
 
 impl<Args> CommandSender<Args> {
+    /// Send a command to the linked `CommandReceiver`.
     pub fn send<C>(&mut self, command: C)
         where C: Command<Args>
     {
@@ -28,9 +38,16 @@ impl<Args> CommandSender<Args> {
     }
 }
 
+/// A `CommandReceiver` is a type that receive commands
+/// from the `CommandSender`.
+/// You can only have one Receiver per stream.
 pub struct CommandReceiver<Args>(SharedBuffer<Args>);
 
 impl<Args> CommandReceiver<Args> {
+    /// Retrieve a command from the pendings command.
+    /// The insertion order is preserved.
+    ///
+    /// Returns None if no command are waiting.
     pub fn recv(&mut self) -> Option<Box<Command<Args>>> {
         self.0.borrow_mut().pop()
     }
@@ -38,23 +55,28 @@ impl<Args> CommandReceiver<Args> {
 
 type SharedBuffer<Args> = Rc<RefCell<CommandBuffer<Args>>>;
 
+/// A simple buffer of commands using a RingBuffer.
 struct CommandBuffer<Args> {
     commands: RingBuf<Box<Command<Args>>>
 }
 
 impl<Args> CommandBuffer<Args> {
+    /// Create a new empty `CommandBuffer`
     fn new() -> CommandBuffer<Args> {
         CommandBuffer {
             commands: RingBuf::new()
         }
     }
 
+    /// Push a new command to the buffer
     fn push<C>(&mut self, command: C)
         where C: Command<Args>
     {
         self.commands.push_back(Box::new(command));
     }
 
+    /// Try to pop command. Returns None if there is no 
+    /// commands in the buffer.
     fn pop(&mut self) -> Option<Box<Command<Args>>> {
         self.commands.pop_front()
     }
