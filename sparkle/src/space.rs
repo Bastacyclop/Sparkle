2cfe4e5af120a0126354db93d57cb17f7618dde2
Liotitch <rph.vaillant@gmail.com>
2015-01-16 16:04:46 +0100

    Add space doc

--- sparkle/src/space.rs
+++ sparkle/src/space.rs
@@ -1,9 +1,14 @@
+//! Main object of the library
+
 use command::{self, CommandReceiver, Command};
 use blackboard::SharedBlackboard;
 use component::ComponentMapper;
 use entity::EntityMapper;
 use system::SystemMapper;
 
+/// A Space is a type that regroups the three essential mappers
+/// and the blackboard. It's also responsible for updating the world
+/// and run commands from command senders.
 pub struct Space {
     cmd_receiver: CommandReceiver<Space>,
     pub blackboard: SharedBlackboard,
@@ -13,6 +18,7 @@ pub struct Space {
 }
 
 impl Space {
+    /// Create a new `Space`
     pub fn new(blackboard: SharedBlackboard) -> Space {
         let (sender, receiver) = command::stream();
 
@@ -25,16 +31,21 @@ impl Space {
         }
     }
 
+    /// Run pending commands and update systems according to the
+    /// given delta time (dt). This should be called every frame.
     pub fn update(&mut self, dt: f32) {
         self.run_commands();
         self.sm.update(&mut self.em, &mut self.cm, dt);
     }
 
+    /// Run pending commands and update systems. This should be called
+    /// at a fixed timestep.
     pub fn fixed_update(&mut self) {
         self.run_commands();
         self.sm.fixed_update(&mut self.em, &mut self.cm);
     }
 
+    /// Run pending commands
     fn run_commands(&mut self) {
         while let Some(mut command) = self.cmd_receiver.recv() {
             command.run(self)
