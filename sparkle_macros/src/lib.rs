f1f48b2160b8e616874a6a1172fd570e3a35a936
Liotitch <rph.vaillant@gmail.com>
2015-01-16 18:24:49 +0100

    Fix sparkle_default_filtering
    It now takes entities and filter ident as parameter to allow systems to
    name these as they want to.

--- sparkle_macros/src/lib.rs
+++ sparkle_macros/src/lib.rs
@@ -32,38 +32,22 @@ pub fn plugin_registrar(reg: &mut plugin::Registry) {
     reg.register_macro("sparkle_filter", expand_filter::expand);
 }
 
-
-
-#[macro_export]
-macro_rules! _sparkle_add_entity {
-    ($system:expr, $mentity:expr) => ({
-        $system.entities.insert($mentity.entity);
-    })
-}
-
-#[macro_export]
-macro_rules! _sparkle_remove_entity {
-    ($system:expr, $mentity:expr) => ({
-        $system.entities.remove(&$mentity.entity);
-    })
-}
-
 #[macro_export]
 macro_rules! sparkle_default_system_filtering {
-    () => (
+    ($entities:ident, $filter:ident) => (
         fn on_entity_changed(&mut self, mentity: &::sparkle::MetaEntity) {
-            let contains = self.entities.contains(&mentity.entity);
-            let pass_filter = self.filter.pass(mentity);
+            let contains = $entities.contains(&mentity.entity);
+            let pass_filter = $filter.pass(mentity);
 
             match (contains, pass_filter) {
-                (true, false) => _sparkle_remove_entity!(self, mentity),
-                (false, true) => _sparkle_add_entity!(self, mentity),
+                (true, false) => { self.$entities.remove(&$mentity.entity); },
+                (false, true) => { self.$entities.insert($mentity.entity); },
                 _ => {}
             }
         }
 
         fn on_entity_removed(&mut self, mentity: &::sparkle::MetaEntity) {
-            _sparkle_remove_entity!(self, mentity);
+            self.$entities.remove(&$mentity.entity);
         }
     )
 }
