526d0dbf7416352b80664a5e8e933298bae04c23
Thomas Koehler <basta.t.k+git@gmail.com>
2015-01-16 21:53:12 +0100

    Review command documentation

--- sparkle/src/command.rs
+++ sparkle/src/command.rs
@@ -1,16 +1,17 @@
-//! The Command related types
+//! The command related features.
 
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 use std::collections::RingBuf;
 
-/// The Command trait represent any command
-/// that can be runned.
+/// Represents any command that can be runned.
 pub trait Command<Args>: 'static {
+    /// Runs the command.
     fn run(&mut self, args: &mut Args);
 }
 
-/// Create a new stream of commands.
+/// Creates a new command stream.
+///
 /// Note that this is not thread safe.
 pub fn stream<Args>() -> (CommandSender<Args>, CommandReceiver<Args>) {
     let buffer = Rc::new(RefCell::new(CommandBuffer::new()));
@@ -18,10 +19,9 @@ pub fn stream<Args>() -> (CommandSender<Args>, CommandReceiver<Args>) {
     (CommandSender(buffer.clone().downgrade()), CommandReceiver(buffer.clone()))
 }
 
-/// A `CommandSender` is a type that send commands
-/// to the `CommandReceiver`.
+/// Sends commands to a `CommandReceiver`.
 ///
-/// You can have multiple sender using the clone method.
+/// You can have multiple senders per stream using the clone method.
 pub struct CommandSender<Args>(WeakSharedBuffer<Args>);
 
 impl<Args> Clone for CommandSender<Args> {
@@ -31,8 +31,9 @@ impl<Args> Clone for CommandSender<Args> {
 }
 
 impl<Args> CommandSender<Args> {
-    /// Send a command to the linked `CommandReceiver`.
-    /// If the receiver is dropped this method does nothing.
+    /// Sends a command to the linked `CommandReceiver`.
+    ///
+    /// If the receiver was dropped, this method does nothing.
     pub fn send<C>(&mut self, command: C)
         where C: Command<Args>
     {
@@ -42,17 +43,17 @@ impl<Args> CommandSender<Args> {
     }
 }
 
-/// A `CommandReceiver` is a type that receive commands
-/// from the `CommandSender`.
+/// Receives commands from `CommandSender`s.
 ///
-/// You can only have one Receiver per stream.
+/// You can only have one receiver per stream.
 pub struct CommandReceiver<Args>(SharedBuffer<Args>);
 
 impl<Args> CommandReceiver<Args> {
-    /// Retrieve a command from the pendings command.
+    /// Retrieves a command from the stream.
+    ///
     /// The insertion order is preserved.
     ///
-    /// Returns None if no command are waiting.
+    /// Returns `None` if there is no more commands to retrieve.
     pub fn recv(&mut self) -> Option<Box<Command<Args>>> {
         self.0.borrow_mut().pop()
     }
@@ -61,28 +62,29 @@ impl<Args> CommandReceiver<Args> {
 type SharedBuffer<Args> = Rc<RefCell<CommandBuffer<Args>>>;
 type WeakSharedBuffer<Args> = Weak<RefCell<CommandBuffer<Args>>>;
 
-/// A simple buffer of commands using a RingBuffer.
+/// A simple buffer of commands using a `RingBuf`.
 struct CommandBuffer<Args> {
     commands: RingBuf<Box<Command<Args>>>
 }
 
 impl<Args> CommandBuffer<Args> {
-    /// Create a new empty `CommandBuffer`
+    /// Creates an empty `CommandBuffer`.
     fn new() -> CommandBuffer<Args> {
         CommandBuffer {
             commands: RingBuf::new()
         }
     }
 
-    /// Push a new command to the buffer
+    /// Pushes a new command into the buffer.
     fn push<C>(&mut self, command: C)
         where C: Command<Args>
     {
         self.commands.push_back(Box::new(command));
     }
 
-    /// Try to pop command. Returns None if there is no 
-    /// commands in the buffer.
+    /// Tries to pop a command.
+    ///
+    /// Returns `None` if the buffer is empty.
     fn pop(&mut self) -> Option<Box<Command<Args>>> {
         self.commands.pop_front()
     }
