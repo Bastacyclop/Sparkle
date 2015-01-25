29e9c0812b7dd1d0e21fc3fd714ff926b0ef2a46
Liotitch <rph.vaillant@gmail.com>
2015-01-25 15:33:22 +0100

    Revert "EntitView now use a BitvSet to store entities"
    This reverts commit e81d5753aec6134bfe0e232a1e9aa5381d846450.

--- sparkle/src/system/filter.rs
+++ sparkle/src/system/filter.rs
@@ -54,7 +54,7 @@
 use std::ops::{Deref};
 use std::collections::{HashSet, BitvSet};
 
-use entity::MetaEntity;
+use entity::{Entity, MetaEntity};
 use component::{Component, index_of};
 
 /// The standard `EntityView`, just an alias.
@@ -64,7 +64,7 @@ pub type StandardEntityView = EntityView<StandardEntityFilter>;
 pub struct EntityView<Filter>
     where Filter: EntityFilter
 {
-    entities: BitvSet,
+    entities: HashSet<Entity>,
     filter: Filter
 }
 
@@ -74,7 +74,7 @@ impl<Filter> EntityView<Filter>
     /// Creates a new `EntityView` with the given `EntityFilter`.
     pub fn new(filter: Filter) -> EntityView<Filter> {
         EntityView {
-            entities: BitvSet::new(),
+            entities: HashSet::new(),
             filter: filter
         }
     }
@@ -105,8 +105,8 @@ impl<Filter> EntityView<Filter>
 impl<Filter> Deref for EntityView<Filter>
     where Filter: EntityFilter
 {
-    type Target = BitvSet;
-    fn deref(&self) -> &BitvSet {
+    type Target = HashSet<Entity>;
+    fn deref(&self) -> &HashSet<Entity> {
         &self.entities
     }
 }
